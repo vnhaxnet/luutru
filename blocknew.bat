0421969d5feba985368e50b898f9eb60308a37c1
vnhaxnet <66384697+vnhaxnet@users.noreply.github.com>
2020-06-10 23:42:21 +0700

    Update blocknew.bat

--- blocknew.bat
+++ blocknew.bat
@@ -10,16 +10,7 @@ adb.exe devices
 adb root
 adb remount
 adb push "C:\Windows\connect.dll" "/etc/hosts"
-
-adb shell iptables -I INPUT -s lobby.igamecj.com -j DROP 
-adb shell iptables -I INPUT -s krlobby.igamecj.com -j DROP 
-adb shell iptables -I INPUT -s twlobby.igamecj.com -j DROP 
-adb shell iptables -I INPUT -s vnglobby.igamecj.com -j DROP 
-adb shell iptables -I INPUT -s 119.28.145.130 -j DROP 
-adb shell iptables -I INPUT -s 119.28.147.30 -j DROP 
-
-adb shell iptables -D INPUT -s 119.28.145.130 -j DROP 
-adb shell iptables -D INPUT -s 119.28.147.30 -j DROP 
-adb shell iptables -A OUTPUT -p tcp --dport 17500 -j DROP 
-adb shell iptables -A OUTPUT -p tcp --dport 35000 -j DROP 
+adb shell iptables -I OUTPUT -p tcp  --dport 17000 -j DROP
+adb shell iptables -I OUTPUT -p tcp  --dport 17500 -j DROP 
+adb shell iptables -I OUTPUT -p tcp  --dport 35000 -j DROP 
 adb shell iptables -L -n -v dpt:17500 
